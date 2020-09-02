69042bfd44a28800d02bde67588eac343b2d7e84
Brian Torres-Gil <brian@ixi.us>
2020-09-01 20:19:46 -0700

    build: Remove excess reveal.js files

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ mkdir -p dist/reveal dist/images
 cp ./*.html dist/
 cp ./*.md dist/
 cp -R images/* dist/images/
-cp -R node_modules/reveal.js/* dist/reveal/
+cp -R node_modules/reveal.js/plugin dist/reveal/plugin
+cp -R node_modules/reveal.js/dist dist/reveal/dist
 sed -i.bak -E "s/\/node_modules\/reveal.js\//reveal\//" "dist/index.html" && rm "dist/index.html.bak"
